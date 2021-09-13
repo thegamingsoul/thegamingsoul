6180cb6cf21ace8d5c33df3af93306bb79f9eef5
Piyush Ramnani <starship@Piyushs-MacBook-Air.local>
2021-09-14 04:56:10 +0530

    First commit

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,16 @@ import { notesToPlayInOrder } from "./music-to-play";
 
 function playMusic() {
     const notes = notesToPlayInOrder;
-    
+
+    // to cycle through the notes automatically in order.
+    for (var key in notes) {
+        if (notes.hasOwnProperty(key)) {
+            return notes[key];
+        }
+    }
+
+    //Also you can play them one by one but it is not an optimal solution.
+
     // TODO Play these notes one after the other at the pitch and rhythm given in each note
 }
-
 document.getElementById('start-playing')?.addEventListener('click', playMusic);
